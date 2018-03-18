70149cfe2fe38ac35f073933af98de9b00423f7e
ranckprogram <1474574022@qq.com>
2018-03-18 12:51:23 +0800

    webstorm github

--- src/router/index.js
+++ src/router/index.js
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Timely',
+      component: Timely
     },
     {
       path: '/timely',
